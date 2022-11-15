215919b9c1f0ce57395ba46c88d15b91ca013c85
Zhang-Setsail <2447086160@qq.com>
2022-11-15 18:32:28 -0500

    openmp code done

--- project3_template_new/src/openmp.cpp
+++ project3_template_new/src/openmp.cpp
@@ -33,14 +33,81 @@ void generate_data(double *m, double *x,double *y,double *vx,double *vy, int n) 
 
 
 
-void update_position(double *x, double *y, double *vx, double *vy, int i) {
+void update_position(double *x, double *y, double *vx, double *vy, int n) {
     //TODO: update position
-
+    int num_thread = omp_get_thread_num();
+    // printf("this is thread : %d/%d\n", num_thread, n_omp_threads);
+    for (int i = num_thread; i < n; i = i + n_omp_threads)
+    {
+        x[i] = x[i] + vx[i] * dt;
+        y[i] = y[i] + vy[i] * dt;
+    }
 }
 
-void update_velocity(double *m, double *x, double *y, double *vx, double *vy, int i) {
+void update_velocity(double *m, double *x, double *y, double *vx, double *vy, int n) {
     //TODO: calculate force and acceleration, update velocity
-
+    int num_thread = omp_get_thread_num();
+    // printf("this is thread : %d/%d\n", num_thread, n_omp_threads);
+    // printf("OVER HERE #3, numthread:%d, n:%d\n", num_thread, n);
+    for (int i = num_thread; i < n; i = i + n_omp_threads)
+    {
+        // printf("OVER HERE #4\n");
+        double acceleration = 0;
+        double acceleration_x = 0;
+        double acceleration_y = 0;
+        // double origin_vx, origin_vy;
+        double x_proj, y_proj, xy_distance_pow;
+        if (x[i] > bound_x || x[i] < 0. || y[i] > bound_y || y[i] < 0.)
+        {
+            vx[i] = -vx[i];
+            vy[i] = -vy[i];
+            // printf("OVER HERE #1\n");
+        }
+        // printf("OVER HERE #1\n");
+        for (int j = 0; j < n; j++)
+        {
+            if (i != j)
+            {
+                xy_distance_pow = pow(x[i] - x[j], 2.) + pow(y[i] - y[j], 2.);
+                // xy_distance_pow = (x[i] - x[j]) * (x[i] - x[j]) + (y[i] - y[j]) * (y[i] - y[j]);
+                // printf("ans111:%f,\n", pow(x[i] - x[j], 2.));
+                // printf("ans2:%f,\n", pow(y[i] - y[j], 2.));
+                // printf("xi:%f, xj:%f, yi:%f, yj:%f\n", x[i], x[j], y[i], y[j]);
+                // printf("xy_dis:%f,\n", xy_distance_pow);
+                if (xy_distance_pow < 1000 * radius2)
+                {
+                    vx[i] = -vx[i];
+                    vy[i] = -vy[i];
+                    acceleration_x = 0;
+                    acceleration_y = 0;
+                    break;
+                }
+                else
+                {
+                    x_proj = pow(pow(x[i] - x[j], 2.) / xy_distance_pow, 0.5);
+                    y_proj = pow(pow(y[i] - y[j], 2.) / xy_distance_pow, 0.5);
+                    acceleration = gravity_const * m[j] / (xy_distance_pow + err);
+                    // printf("acc:%f, xP:%f, yP:%f\n", acceleration, x_proj, y_proj);
+                    if (x[i] < x[j])
+                    {
+                        acceleration_x = acceleration_x + acceleration * x_proj;
+                    } else {
+                        acceleration_x = acceleration_x - acceleration * x_proj;
+                    }
+                    if (y[i] < y[j])
+                    {
+                        acceleration_y = acceleration_y + acceleration * y_proj;
+                    } else {
+                        acceleration_y = acceleration_y - acceleration * y_proj;
+                    }
+                }
+            }
+        }
+        // printf("acc_X:%f, acc_Y%f\n", acceleration_x, acceleration_y);
+        vx[i] = vx[i] + acceleration_x * dt;
+        vy[i] = vy[i] + acceleration_y * dt;
+        // printf("v_X:%f, v_Y%f\n\n", vx[i], vy[i]);
+    }
 }
 
 
@@ -60,15 +127,20 @@ void master() {
         
         //TODO: choose better threads configuration
         omp_set_num_threads(n_omp_threads);
-        #pragma omp parallel for
-        for (int i = 0; i < n_body; i++) {
-            update_velocity(m, x, y, vx, vy, i);
-        }
+        int nb = n_body;
+        #pragma omp parallel
+        // for (int i = 0; i < n_body; i++) {
+            {
+            update_velocity(m, x, y, vx, vy, nb);
+            }
+        // }
 
-        omp_set_num_threads(8);
-        #pragma omp parallel for
-        for (int i = 0; i < n_body; i++) {
-            update_position(x, y, vx, vy, i);
+        omp_set_num_threads(n_omp_threads);
+        #pragma omp parallel
+        {
+        // for (int i = 0; i < n_body; i++) {
+            update_position(x, y, vx, vy, nb);
+        // }
         }
 
         std::chrono::high_resolution_clock::time_point t2 = std::chrono::high_resolution_clock::now();
